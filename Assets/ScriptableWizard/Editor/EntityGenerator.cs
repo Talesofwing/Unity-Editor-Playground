bbe6b3e6ddd89de94493534ba0c3d4917eb30152
zer0 <zerox0103@gmail.com>
2023-04-22 22:05:06 +0900

    Add ScriptableWizard Example

---
+++ Assets/ScriptableWizard/Editor/EntityGenerator.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace zer0.Editor {
+
+	public class EntityGenerator : ScriptableWizard {
+		[SerializeField] private string _name;
+		[SerializeField] private int _hp;
+		[SerializeField] private int _atk;
+
+		// default values
+		private const string DefaultName = "zer0";
+		private const int DefaultHp = 1;
+		private const int DefaultAtk = 1;
+
+		/// <summary>
+		/// It will be called when the window open
+		/// </summary>
+		private void Awake () {
+			Debug.Log ("Awake");
+
+			_name = DefaultName;
+			_hp = DefaultHp;
+			_atk = DefaultAtk;
+		}
+
+		#region ScriptableWizard
+
+		[MenuItem ("zer0/ScriptableWizard/Entity Generator")]
+		private static void CreateWizard () {
+			// Title, Create Button Name, Other Button Name
+			DisplayWizard<EntityGenerator> ("Entity Generator", "Generate", "Reset");
+		}
+
+		/// <summary>
+		/// On create button click
+		/// </summary>
+		private void OnWizardCreate () {
+			GameObject go = new GameObject (_name);
+			Entity entity = go.AddComponent<Entity> ();
+			entity.Init (_name, _hp, _atk);
+
+			Debug.Log ($"{_name} created!");
+		}
+
+		/// <summary>
+		/// On other button click
+		/// </summary>
+		private void OnWizardOtherButton () {
+			_name = DefaultName;
+			_hp = DefaultHp;
+			_atk = DefaultAtk;
+
+			Debug.Log ("Reset");
+		}
+
+		/// <summary>
+		/// On wizard update
+		/// </summary>
+		private void OnWizardUpdate () {
+			// do something
+		}
+
+	}
+
+	#endregion
+
+}
--- Docs/ScriptableWizard.md
+++ Docs/ScriptableWizard.md
@@ -1,1 +1,42 @@
 # ScriptableWizard
+
+## What is Wizard?
+
+In computer science, `Wizard` refers to a type of user interface element that provides step-by-step guidance to the user.
+
+`ScriptableWizard` is the Unity version of the `Wizard`.
+
+## Unity Wizard (ScriptableWizard)
+
+```csharp
+using UnityEditor;
+
+public class MyWizard : ScriptableWizard { }
+```
+
+Inherit from `ScriptableWizard` like the code above👆. It consists of four parts,
+
+![ScriptableWizard](/Images/001.png)
+
+and five functions.
+
+```csharp
+    [MenuItem ("zer0/ScriptableWizard/MyWizard")]
+    private static void CreateWizard () {
+        DisplayWizard<MyWizard> ("Title", "Create Button", "Other Button");
+    }
+
+    // It will be called when the wizard open.
+    // Note: Wizard only have the Awake() function and don't have Start() function or any others.
+    private void Awake () {}
+
+    // On create button click.
+    private void OnWizardCreate () {}
+
+    // On other button click.
+    private void OnWizardOtherButton () {}
+
+    // It will be called once when the wizard open.
+    // It will be called when propeties are changed.
+    private void OnWizardUpdate () {}
+```

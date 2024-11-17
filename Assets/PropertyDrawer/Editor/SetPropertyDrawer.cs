0aca6458f1be7dcbfe1b88d2ae803c2c0f5d9e3f
zer0 <zerox0103@gmail.com>
2024-11-17 09:54:04 +0900

    Reformat SetPropertyAttribute

--- Assets/PropertyDrawer/Editor/SetPropertyDrawer.cs
+++ Assets/PropertyDrawer/Editor/SetPropertyDrawer.cs
@@ -6,28 +6,29 @@ using UnityEditor;
 
 namespace zer0.Editor {
 
-	[CustomPropertyDrawer (typeof (SetPropertyAttribute))]
-	public class SetPropertyDrawer : PropertyDrawer {
+    [CustomPropertyDrawer(typeof(SetPropertyAttribute))]
+    public class SetPropertyDrawer : PropertyDrawer {
 
-		public override void OnGUI (Rect position, SerializedProperty property, GUIContent label) {
-			EditorGUI.BeginChangeCheck ();
-			EditorGUI.PropertyField (position, property, label);
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            EditorGUI.BeginChangeCheck();
+            EditorGUI.PropertyField(position, property, label);
 
-			SetPropertyAttribute setProperty = attribute as SetPropertyAttribute;
-			if (EditorGUI.EndChangeCheck ()) {
-				setProperty.IsDirty = true;
-			} else if (setProperty.IsDirty) {
-				object parent = property.serializedObject.targetObject;
-				Type type = parent.GetType ();
+            SetPropertyAttribute setProperty = attribute as SetPropertyAttribute;
+            if (EditorGUI.EndChangeCheck()) {
+                setProperty.IsDirty = true;
+            } else if (setProperty.IsDirty) {
+                object parent = property.serializedObject.targetObject;
+                Type type = parent.GetType();
 
-				PropertyInfo pi = type.GetProperty (setProperty.Name);
-				if (pi == null) Debug.LogError ("Invalied property name: " + setProperty.Name);
-				else pi.SetValue (parent, fieldInfo.GetValue (parent), null);
+                PropertyInfo pi = type.GetProperty(setProperty.Name);
+                if (pi == null) {
+                    Debug.LogError("Invalied property name: " + setProperty.Name);
+                } else {
+                    pi.SetValue(parent, fieldInfo.GetValue(parent), null);
+                }
 
-				setProperty.IsDirty = false;
-			}
-		}
-
-	}
-
+                setProperty.IsDirty = false;
+            }
+        }
+    }
 }
--- Assets/PropertyDrawer/Scripts/SetPropertyAttribute.cs
+++ Assets/PropertyDrawer/Scripts/SetPropertyAttribute.cs
@@ -2,14 +2,12 @@ using UnityEngine;
 
 namespace zer0 {
 
-	public class SetPropertyAttribute : PropertyAttribute {
-		public string Name { get; private set; }
-		public bool IsDirty { get; set; }
+    public class SetPropertyAttribute : PropertyAttribute {
+        public string Name { get; private set; }
+        public bool IsDirty { get; set; }
 
-		public SetPropertyAttribute (string name) {
-			this.Name = name;
-		}
-
-	}
-
+        public SetPropertyAttribute(string name) {
+            this.Name = name;
+        }
+    }
 }
